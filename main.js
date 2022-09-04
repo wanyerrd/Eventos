b9bce3ca81f0049487f890341bfb61ec65287791
wanyerrd <111544447+wanyerrd@users.noreply.github.com>
2022-09-04 16:29:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,11 +5,33 @@
 
 const nombre = document.getElementById ("Nombre");
 
+const Apellido = document.getElementById ("Apellido");
+
+const email = document.getElementById ("Correo");
+
+const Contraseña = document.getElementById ("Contraseña");
+
 const btn = document.getElementById("btn");
 btn.addEventListener("click" , (e) => {
 e.preventDefault();
- alert(nombre.value);
-})
+ console.log("Tu nombre es:" +nombre.value);
+ console.log("Tu apellido es:" +Apellido.value);
+ console.log("Tu Correo es:" +email.value);
+ console.log("Tu contraseña es:"+Contraseña.value);
+
+ 
+nombre.value = ""
+Apellido.value = ""
+email.value = ""
+Contraseña.value = ""
+});
+
+
+
+
+
+
+
 
 
 
